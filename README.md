c2c4caf69467eb43ef9a40b6be9c2ba24f561d65
cwj <282330332@qq.com>
2025-03-25 19:53:14 +0800

    相关资料同步

--- README.md
+++ README.md
@@ -4,3 +4,14 @@
 Android-Handler消息机制实现原理
 https://www.cnblogs.com/it-tsz/p/11084995.html
 
+源码深度解析 Handler 机制及应用
+https://blog.51cto.com/u_14291117/6168665
+
+
+
+
+
+
+
+
+
--- app/src/main/java/com/example/handlerdemo/MainActivity.kt
+++ app/src/main/java/com/example/handlerdemo/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.handlerdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    /**
+     *
+     */
+    fun onTest1(v : View) {
+
+    }
+
 }
